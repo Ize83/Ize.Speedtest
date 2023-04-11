115bb49b18660caa18e0893bf90a498593a5c137
Ize83 <izegod@t-online.de>
2023-04-11 11:53:30 +0200

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,71 @@
+﻿using SuchByte.MacroDeck;
+using SuchByte.MacroDeck.ActionButton;
+using SuchByte.MacroDeck.GUI;
+using SuchByte.MacroDeck.GUI.CustomControls;
+using SuchByte.MacroDeck.Logging;
+using SuchByte.MacroDeck.Plugins;
+using SuchByte.MacroDeck.Variables;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using SpeedTest.Net.Enums;
+using SpeedTest.Net.Models;
+using SpeedTest.Net;
+
+namespace Ize.Speedtest_Plugin
+{
+    public class SpeedtestPlugin : MacroDeckPlugin
+    {
+        internal static MacroDeckPlugin Instance { get; set; }
+
+        public SpeedtestPlugin()
+        {
+            Instance = this;
+        }
+
+        public override void Enable()
+        {
+            this.Actions = new List<PluginAction>
+            {
+                new Speedtest(),
+            };
+        }
+    }
+
+    public class Speedtest : PluginAction
+    {
+        public override string Name => "Speedtest";
+        public override string Description => "Runs a speedtest and updates the speedtest variables...";
+        public override void Trigger(string clientId, ActionButton actionButton)
+        {
+            VariableManager.SetValue("SpeedtestActive", true, VariableType.Bool, SpeedtestPlugin.Instance, true);
+            ExecuteSpeedTest();
+        }
+
+        private async void ExecuteSpeedTest()
+        {
+            try
+            {
+                DownloadSpeed speed = null;
+
+                speed = await FastClient.GetDownloadSpeed(SpeedTestUnit.KiloBitsPerSecond);
+
+                var message = $"Source: {speed.Source} Download Speed: {speed?.Speed} {speed.Unit}";
+
+                if (speed?.Server?.Id != null)
+                    message += $" (Server Id = {speed?.Server?.Id})";
+
+                VariableManager.SetValue("SpeedtestDownload", speed.Speed, VariableType.Float, SpeedtestPlugin.Instance, true);
+                VariableManager.SetValue("SpeedtestActive", false, VariableType.Bool, SpeedtestPlugin.Instance, true);
+
+            }
+            catch (System.Exception ex)
+            {
+                MacroDeckLogger.Error(SpeedtestPlugin.Instance, ex.Message);
+            }
+        }
+    }
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# MacroDeck-Speedtest-Plugin
+Simple Speedtest Plugin for Macro-Deck
+
+Work in progress but it already works.
+
+Powered by SpeedTest.Net: https://github.com/JunaidAK/SpeedTest.Net
