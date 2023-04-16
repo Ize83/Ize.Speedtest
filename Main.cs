54e3587f5f171e7d3e59ecd30c225cf347faa07a
Manuel Mayer <suchbyte@gmail.com>
2023-04-16 23:57:50 +0200

    Refactoring the project Added unit varible Changed unit from kbps to Mbps

--- Main.cs
+++ Main.cs
@@ -1,16 +1,7 @@
-﻿using SuchByte.MacroDeck;
-using SuchByte.MacroDeck.ActionButton;
-using SuchByte.MacroDeck.GUI;
-using SuchByte.MacroDeck.GUI.CustomControls;
-using SuchByte.MacroDeck.Logging;
+﻿using Speedtest_Plugin;
+using Speedtest_Plugin.Actions;
 using SuchByte.MacroDeck.Plugins;
-using SuchByte.MacroDeck.Variables;
-using System;
 using System.Collections.Generic;
-using System.Drawing;
-using SpeedTest.Net.Enums;
-using SpeedTest.Net.Models;
-using SpeedTest.Net;
 
 namespace Ize.Speedtest_Plugin
 {
@@ -27,40 +18,11 @@ namespace Ize.Speedtest_Plugin
         {
             this.Actions = new List<PluginAction>
             {
-                new Speedtest(),
+                new SpeedtestAction(),
             };
+
+            VariableHelper.UpdateState(false);
+            SpeedtestHelper.ExecuteSpeedTest();
         }
     }
-
-    public class Speedtest : PluginAction
-    {
-        public override string Name => "Speedtest";
-        public override string Description => "Runs a speedtest and updates the speedtest variables.";
-        public override void Trigger(string clientId, ActionButton actionButton)
-        {
-            VariableManager.SetValue("SpeedtestActive", true, VariableType.Bool, SpeedtestPlugin.Instance, true);
-            ExecuteSpeedTest();
-        }
-
-        private async void ExecuteSpeedTest()
-        {
-            try
-            {
-                DownloadSpeed speed = null;
-
-                speed = await FastClient.GetDownloadSpeed(SpeedTestUnit.KiloBitsPerSecond);
-
-                VariableManager.SetValue("SpeedtestDownload", speed.Speed, VariableType.Float, SpeedtestPlugin.Instance, true);
-                VariableManager.SetValue("SpeedtestActive", false, VariableType.Bool, SpeedtestPlugin.Instance, true);
-
-            }
-            catch (System.Exception ex)
-            {
-                MacroDeckLogger.Error(SpeedtestPlugin.Instance, ex.Message);
-            }
-        }
-    }
-
-
-
 }
---
+++ SpeedtestHelper.cs
@@ -0,0 +1,45 @@
+﻿using Ize.Speedtest_Plugin;
+using SpeedTest.Net.Enums;
+using SpeedTest.Net;
+using SuchByte.MacroDeck.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Speedtest_Plugin
+{
+    public static class SpeedtestHelper
+    {
+        private static bool _speedTestRunning = false;
+
+        public static void ExecuteSpeedTest()
+        {
+            Task.Run(async () => await ExecuteSpeedTestAsync());
+        }
+
+        private static async Task ExecuteSpeedTestAsync()
+        {
+            if (_speedTestRunning)
+            {
+                return;
+            }
+
+            _speedTestRunning = true;
+            VariableHelper.UpdateState(true);
+
+            try
+            {
+                var download = await FastClient.GetDownloadSpeed(SpeedTestUnit.MegaBitsPerSecond);
+                VariableHelper.UpdateDownloadSpeed(download.Speed, download.Unit);
+            }
+            catch (Exception ex)
+            {
+                MacroDeckLogger.Error(SpeedtestPlugin.Instance, ex.StackTrace);
+            }
+            finally
+            {
+                _speedTestRunning = false;
+                VariableHelper.UpdateState(false);
+            }
+        }
+    }
+}
